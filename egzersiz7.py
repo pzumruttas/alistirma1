21a8f10273bc2816c049765b55a96a1f4f710fcd
pzumruttas <56044119+pzumruttas@users.noreply.github.com>
2019-10-20 12:13:38 +0300

    Add files via upload

---
+++ egzersiz7.py
@@ -0,0 +1,9 @@
+#1. ve 2. basamaktaki rakamları toplamı 3. basamktakine eşit olan 3 basamaklı sayıların sayısını ve kendilerini bastırır
+sayac=0
+for sayi in range(100,1000):
+    sayi_str=str(sayi)
+    if int(sayi_str[0])+int(sayi_str[1])==int(sayi_str[2]):
+        sayac=sayac+1
+        print(sayi)
+
+print(sayac)
