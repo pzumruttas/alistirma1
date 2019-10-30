7b228ee82c96e82284f6a5a1d9ee12ff91d4ff1a
pzumruttas <56044119+pzumruttas@users.noreply.github.com>
2019-10-30 10:10:16 +0300

    Update egzersiz9.py

--- egzersiz9.py
+++ egzersiz9.py
@@ -2,8 +2,8 @@ toplam=0
 
 for sayi in range(1,1000):
     str_sayi=str(sayi)
-    for i in range(1,len(str_sayi)+1):
-        toplam=toplam+int(str_sayi[i-1])
+    for i in range(0,len(str_sayi)):
+        toplam=toplam+int(str_sayi[i])
     if toplam<9:
         print(sayi, end=" ")
     toplam=0
