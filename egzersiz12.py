e6bd6e692f7469146505bfdfb899c08ae16045c5
pzumruttas <56044119+pzumruttas@users.noreply.github.com>
2019-10-30 10:24:17 +0300

    Update egzersiz12.py

--- egzersiz12.py
+++ egzersiz12.py
@@ -2,8 +2,8 @@ toplam=0
 
 for yil in range(1000,2005):
     str_yil=str(yil)
-    for i in range(1,5):
-        toplam=toplam+int(str_yil[i-1])
+    for i in range(0,4):
+        toplam=toplam+int(str_yil[i])
     if (2005-yil)==toplam:
         print(yil)
     toplam=0
