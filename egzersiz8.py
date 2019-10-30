070f8c6da86b81fb280f2c9e1840a391e583801c
pzumruttas <56044119+pzumruttas@users.noreply.github.com>
2019-10-30 10:07:37 +0300

    Update egzersiz8.py

--- egzersiz8.py
+++ egzersiz8.py
@@ -3,4 +3,4 @@
 for sayi in range(100,1000,2):
     sayi_str=str(sayi)
     if sayi_str[0]==sayi_str[1] or sayi_str[0]==sayi_str[2] or sayi_str[1]==sayi_str[2]:
-        print(int(sayi_str))
+        print(sayi)
