6ca76dd9e2bcdeb32fcea44b33567cfb5ea5d18c
pzumruttas <56044119+pzumruttas@users.noreply.github.com>
2019-11-06 21:58:39 +0300

    Update egzersiz7.py

--- egzersiz7.py
+++ egzersiz7.py
@@ -4,6 +4,6 @@ for sayi in range(100,1000):
     sayi_str=str(sayi)
     if int(sayi_str[0])+int(sayi_str[1])==int(sayi_str[2]):
         sayac=sayac+1
-        print(sayi)
+        print(sayi,end=" ")
 
 print(sayac)
